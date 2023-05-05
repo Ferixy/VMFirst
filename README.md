3ed767144989cf9dbe72f19b2b68b608ebeed549
Ferixy | Amirmahdi Samsami <77561374+Ferixy@users.noreply.github.com>
2023-05-05 17:00:01 +0330

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # VMFirst
- An small and pretty basic cmd script for fast first time setup of new VMs.
+
+[![Telegram Badge](https://img.shields.io/badge/-HurbSquad-2ca5e0?style=flat&logo=telegram&logoColor=white&link=https://www.t.me/al1almasi)](https://www.t.me/HurbSquad)
+
+VMFirst is a small stupid cmd script created out of pure frustration over day to day need of configuring new made VMs. this script is designed around requiring almost zero user input to save time and confusion, they focus of the script is to get rid of the unnecessary windows bloatware and features that are not needed in a VM environment.
+
+![VMFirst Screenshot](https://i.imgur.com/7iYThss.png)
+
+## Why use it
+
+The main advantage of VMFirst is ease of use and the time you can save with it. It is designed to let you focus on real important stuff while it gets the vm ready for you, which makes it great for **Mass VM installing** or **Repeated VM installation**.
+
+I made this script for myself and my friends, but I decided to upload it here. I hope you find it useful as well.
+
+## Usage
+### Note: Run the script as administrator
+
+1. Download the [latest release](https://github.com/ferixy/vmfirst/releases/)
+2. Again, please run the script as administrator
+3. enjoy!
+
+## VMFirst Features
+
+
+| Argument                  | Description                                                                              |
+| ------------------------- | -----------------------------------------------------------------------                  |
+| `Disable News and interest`     | Disables News and interests and widget.               |
+| `Disable Advertising ID`                      | Disables and resets the microsoft advertising ID |
+| `Disable Automatic Windows Updates`   | Disables Windows Automatic updates                                       |
+| `Disable BingSearch` | Disables windows search with bing                                |
+| `Disable Cortana`                 | Disables Cortana                                                             |
+| `Disable MeetNow`     | Disables MeetNow button and adverting                                                                 |
+| `Disable Mouse acceleration`       | Disables Mouse acceleration                                                              |
+| `Disable SearchBar` | Disables the big ol search windows search bar on taskbar                                                         |
+| `Disable Automatic download of promoted apps` | Disables the big ol windows search bar on taskbar                                                         | 
+| `Disable telemetry` | Disables the nasty microsoft phone home thingy                                                        |
+| `Disable Windows Defender` | Disables Windows Defender and its services to reduce cpu and disk usage                                                        |
+| `Show file extensions ` | Makes explorer show file extension.                                                        |
+| `Show hidden files` | Makes explorer show hidden files.                                                        |
+| `Uninstall OneDrive` | Uninstalls OneDrive                                                        |
+| `ReEnable Defender` | Incase if you want to reactivate windows defender, there is the option to reset windows defender and its services                                                        |
+
+
+## Things to consider
+
+- This script applies the changes in user(HKEY_CURRENT_USER) registry, it is recommended to run it on every user account on your VM.
+- this script is intended to be used on a windows 10 VMs but you can try it on Windows 11 if you are feeling brave.
+- you can run and use VMFirst as soon as you install windows, but it is recommended to run VMFirst after you install VMware/VirtualBox drivers on your OS.
---
+++ VMFirst.cmd
@@ -0,0 +1,105 @@
+@echo off
+:: Check if running as admin
+NET SESSION >NUL 2>&1
+if %errorLevel% == 0 (
+  echo Running with admin rights...
+) else (
+  echo Please run the script as admin.
+  pause
+  exit /b 1
+)
+:menu
+cls
+echo   dBP dP      dBBBBBBb     dBBBBP     dBP    BBBBBb  .dBBBBP  dBBBBBBP
+echo                '   dB'                          dBP  BP               
+echo  dB .BP     dB'dB'dB'    dBBBP      dBP     dBBBBK   `BBBBb    dBP    
+echo  BB.BP     dB'dB'dB'    dBP        dBP     dBP  BB      dBP   dBP     
+echo  BBBP     dB'dB'dB'    dBP        dBP     dBP  dB' dBBBBP'   dBP                                                                                                                        
+echo.
+echo.
+echo An small and pretty basic script for fast first time setup of new VMs. made by Ferixy@Github
+color 4
+echo Please note that this script disables windows defender to reducing cpu/disk usage of the vm but it's Highly recommended to turn it back back on if you are using this script on a real machine.
+echo.
+echo Choose an option:
+echo 1. Start Debloating
+echo 2. Reenable Windows defender
+echo 0. Exit
+
+set /p choice=Type the number of your choice:
+
+if "%choice%"=="1" goto option1
+if "%choice%"=="2" goto option2
+if "%choice%"=="0" goto endtask
+
+:option1
+echo Debloating.
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Feed" /v "ShellFeedsTaskbarViewMode" /t REG_DWORD /d 2 /f
+echo News and interests has been disabled.
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v "Enabled" /t REG_DWORD /d 0 /f
+echo Advertising ID has been disabled.
+set "key=HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows"
+reg add "%key%\WindowsUpdate" /v "" /t REG_SZ /d "" /f >nul
+reg add "%key%\WindowsUpdate\AU" /v "" /t REG_SZ /d "" /f >nul
+reg add "%key%\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f >nul
+echo Windows automatic updates have been disabled.
+reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableSearchBoxSuggestions" /t REG_DWORD /d 1 /f
+echo BingSearch has been disabled.
+reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "Windows Search" /t REG_DWORD /d 0 /f
+echo Cortana has been disabled.
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "HideSCAMeetNow" /t REG_DWORD /d 1 /f
+echo MeetNow button has been disabled.
+reg add "HKCU\Control Panel\Mouse" /v "MouseSpeed" /t REG_DWORD /d 0 /f
+reg add "HKCU\Control Panel\Mouse" /v "MouseThreshold1" /t REG_DWORD /d 0 /f
+reg add "HKCU\Control Panel\Mouse" /v "MouseThreshold2" /t REG_DWORD /d 0 /f
+echo Mouse acceleration has been disabled.
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 0 /f
+echo SearchBar has been disabled.
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion" /v "SilentInstalledAppsEnabled" /t REG_DWORD /d 0 /f
+echo Automatic download of promoted apps has been disabled.
+reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0 /f
+echo telemetry has been disabled.
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d 0 /f
+echo explorer now shows file extensions.
+echo Killing OneDrive...
+tasklist | find /i "onedrive.exe" >nul
+
+if errorlevel 1 (
+    echo OneDrive is not running. skipping OneDrive uninstallation...
+) else (
+    taskkill /f /im "onedrive.exe"
+    echo OneDrive has been terminated.
+    ping -n 2 127.0.0.1 > nul
+echo Uninstalling OneDrive... This will take around 10 seconds.
+%systemroot%\SysWOW64\OneDriveSetup.exe /uninstall /quiet /norestart
+ping -n 10 127.0.0.1 > nul
+echo OneDrive Uninstalled.
+)
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d 1 /f
+echo NOTE: Please ignore the error messages below(if any appeared).
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "Windows Defender" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /v "Windows Defender" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "WindowsDefender" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d 1 /f
+echo Windows defender has been disabled!!!.
+echo.
+echo PLEASE RESTART YOUR SYSTEM FOR CHANGES TO TAKE EFFECT.
+pause
+goto menu
+
+:option2
+echo About to reenable windows defender...
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d 1 /f
+echo Windows defender has been reactivated. PLEASE RESTART YOUR SYSTEM FOR CHANGES TO TAKE EFFECT.
+echo PLEASE RESTART YOUR SYSTEM FOR CHANGES TO TAKE EFFECT.
+echo PLEASE RESTART YOUR SYSTEM FOR CHANGES TO TAKE EFFECT.
+echo PLEASE RESTART YOUR SYSTEM FOR CHANGES TO TAKE EFFECT.
+echo PLEASE RESTART YOUR SYSTEM FOR CHANGES TO TAKE EFFECT.
+echo PLEASE RESTART YOUR SYSTEM FOR CHANGES TO TAKE EFFECT.
+echo PLEASE RESTART YOUR SYSTEM FOR CHANGES TO TAKE EFFECT.
+echo PLEASE RESTART YOUR SYSTEM FOR CHANGES TO TAKE EFFECT.                                                                                                                                                                                                                                                                                 
+pause
+goto menu
+
+:endtask
+echo Exiting...
